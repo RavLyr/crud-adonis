14c1f56cc16ed3f0cafa237afb7ade61bf52939d
RavLyr <ravlybrb@gmail.com>
2023-09-18 11:46:48 +0700

    #1 - fix - Change column name on migration

--- database/migrations/1694589634313_users.ts
+++ database/migrations/1694589634313_users.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
       table.string("nama");
-      table.integer("nomor_telfon");
+      table.integer("nomorTelfon");
       table.enu("gender", ["L", "P"]);
 
       /**
